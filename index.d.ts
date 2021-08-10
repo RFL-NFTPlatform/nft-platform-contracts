9911f90e29965885b50b099036a090a5a2e5bb38
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-08-10 08:47:56 +0200

    Improve ABI types

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,37 @@
 
+export type Param = {
+    internalType: string,
+    name: string,
+    type: string,
+}
+
+export type ConstructorType = {
+    type: 'constructor',
+    inputs: Param[],
+    stateMutability: string
+}
+
+export type EventType = {
+    type: 'event',
+    name: string,
+    inputs: Param[],
+    anonymous: boolean,
+}
+
+export type FunctionType = {
+    type: 'function',
+    name: string,
+    inputs: Param[],
+    outputs: Param[],
+    stateMutability: string
+}
+
 export const RFOXCollection: {
     contractName: string,
-    abi: {
-        inputs: {
-            internalType: string,
-            name: string,
-            type: string,
-        }[],
-        name?: string,
-        outputs?: any[],
-        stateMutability: string,
-        type: string,
-    }[],
+    abi: (ConstructorType | EventType | FunctionType)[],
     metadata: string,
     bytecode: string,
     deployedBytecode: string,
+    linkReferences: any,
+    deployedLinkReferences: any,
 };
