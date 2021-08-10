c26f058499a584f9428240d2136d12fb375a14a5
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-08-10 12:00:35 +0200

    Add rinkeby conf

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,16 @@ require('@nomiclabs/hardhat-ethers')
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    rinkeby: {
+      url: 'https://rinkeby.infura.io/v3/' + process.env.INFURA_KEY,
+      chainId: 4,
+      from: process.env.OWNER_ADDRESS,
+      gas: 5000000,
+      accounts: {
+        mnemonic: process.env.MNEMONIC
+      },
+    }
+  },
   solidity: "0.8.0",
 };
