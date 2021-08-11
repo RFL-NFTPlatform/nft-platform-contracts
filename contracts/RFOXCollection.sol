2b4870e502021a4c59330ae2d1f9056d5c9ae39b
Luis Mastrangelo <luismastrangelo@gmail.com>
2021-08-11 15:40:11 +0200

    Add proxy address to support gasless listings

--- contracts/RFOXCollection.sol
+++ contracts/RFOXCollection.sol
@@ -7,16 +7,25 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+contract OwnableDelegateProxy {}
+
+contract ProxyRegistry {
+    mapping(address => OwnableDelegateProxy) public proxies;
+}
+
 contract RFOXCollection is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
     string private _contractURI;
 
+    address private _proxyRegistryAddress;
+ 
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor(string memory contractURI_, string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+    constructor(string memory contractURI_, string memory name_, string memory symbol_, address proxyRegistryAddress_) ERC721(name_, symbol_) {
         _contractURI = contractURI_;
+        _proxyRegistryAddress = proxyRegistryAddress_;
     }
 
     function contractURI() public view returns (string memory) {
@@ -62,4 +71,18 @@ contract RFOXCollection is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     {
         return super.supportsInterface(interfaceId);
     }
+
+    /**
+     * Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-less listings.
+     */
+    function isApprovedForAll(address owner, address operator) override public view returns (bool) {
+        // Whitelist OpenSea proxy contract for easy trading.
+        ProxyRegistry proxyRegistry = ProxyRegistry(_proxyRegistryAddress);
+        if (address(proxyRegistry.proxies(owner)) == operator) {
+            return true;
+        }
+
+        return super.isApprovedForAll(owner, operator);
+    }
+
 }
--- test/RFOXCollection.test.js
+++ test/RFOXCollection.test.js
@@ -1,12 +1,13 @@
 const { expect } = require('chai');
 
 describe("RFOXCollection Contract", () => {
+  const proxyRegistryAddress = '0xf57b2c51ded3a29e6891aba85459d600256cf317';
   const CONTRACT_URI = 'ipfs://QmYSrTKu4GQoKSH17wn5SsCVEaEQMwxDRsTfwrzD43HWRD';
   let contract;
 
   before(async () => {
     const RFOXCollection = await ethers.getContractFactory("RFOXCollection");
-    contract = await RFOXCollection.deploy(CONTRACT_URI, 'RFOX Test Collection', 'TST');
+    contract = await RFOXCollection.deploy(CONTRACT_URI, 'RFOX Test Collection', 'TST', proxyRegistryAddress);
   });
 
   it('should set the constructor args to the supplied value', async () => {
@@ -17,7 +18,7 @@ describe("RFOXCollection Contract", () => {
 
   it('should get URI for minted tokens', async () => {
     for (let i = 0; i < 10; i++) {
-      const hash =  `QmYSrTKu4GQoKSH17wn5SsCVEaEQMwxDRsTfwrzD43HWR${i}`;
+      const hash = `QmYSrTKu4GQoKSH17wn5SsCVEaEQMwxDRsTfwrzD43HWR${i}`;
       await contract.safeMint('0xB9cD322bbC641B93e9A9F4262283Fd30C44c35d9', hash);
       expect(await contract.tokenURI(i)).to.equal(`ipfs://${hash}`);
     }
