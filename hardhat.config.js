e8b2c84b112188b9ea93fdcf6c718ec899fc5f61
Luis Mastrangelo <acuarica@users.noreply.github.com>
2021-09-20 15:50:59 +0200

    Test coverage (#3)
    * Add test coverage & test coverage GH pages
    
    * Add GH pages
    
    * Refactor gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require('@nomiclabs/hardhat-waffle');
-require("@nomiclabs/hardhat-etherscan");
+require('@nomiclabs/hardhat-etherscan');
+require('solidity-coverage');
 
 const networks = process.env.INFURA_KEY && process.env.OWNER_ADDRESS && process.env.MNEMONIC ?
   {
